eddcdc2b701677ffdc9b5de411f8cb51f4fa2a35
Fallen_Breath <yqx791125833@gmail.com>
2022-07-23 16:20:13 +0800

    1.17 is different

--- scripts/constant.py
+++ scripts/constant.py
@@ -13,7 +13,6 @@ assert DATA_DIR.is_dir()
 __meta = utils.load_yaml(DATA_DIR / 'meta.yml')
 LANGUAGES: List[str] = __meta['languages']
 DEFAULT_LANGUAGE: str = LANGUAGES[0]
-IMPORTANT_PHASES: List[str] = __meta['important_phases']
 
 
 class MCVersion(NamedTuple):
